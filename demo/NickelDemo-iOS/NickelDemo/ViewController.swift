3a090705494e58715782a5dddca9c11e787fb857
riana <r.ralambo@gmail.com>
2016-01-23 11:49:53 +0100

    Add demo to project

---
+++ demo/NickelDemo-iOS/NickelDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  NickelDemo
+//
+//  Created by Riana Ralambomanana on 23/01/2016.
+//  Copyright © 2016 riana.io. All rights reserved.
+//
+
+import UIKit
+import NickelSwift
+
+class ViewController: NickelWebViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setMainPage("www/index")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
