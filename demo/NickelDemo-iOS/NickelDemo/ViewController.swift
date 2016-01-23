82a2f77763c19fcd174a4df31846daf1d2295c22
riana <r.ralambo@gmail.com>
2016-01-24 00:10:15 +0100

    Move Store to library

--- demo/NickelDemo-iOS/NickelDemo/ViewController.swift
+++ demo/NickelDemo-iOS/NickelDemo/ViewController.swift
@@ -12,15 +12,9 @@ import SwiftyJSON
 
 class ViewController: NickelWebViewController {
     
-    var store:NickelStore?;
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setMainPage("www/index")
-//        NickelStore.drop()
-        store = NickelStore()
-        
     }
     
     override func didReceiveMemoryWarning() {
@@ -30,44 +24,9 @@ class ViewController: NickelWebViewController {
     
     // The wiew has loaded
     override func didFinishLoading() {
-        registerBridgedFunction("storeObject", bridgedMethod: self.storeObject)
-        registerBridgedFunction("loadAllObjects", bridgedMethod: self.loadAllObjects)
-        registerBridgedFunction("deleteObject", bridgedMethod: self.deleteObject)
-        
-        
-    }
-    
-    func storeObject(operation:String, content:[NSObject:AnyObject]) -> [NSObject:AnyObject]?{
-        let type = content["type"] as! String
-        let id = content["id"] as! String
-        let data = content["data"] as! String
-        let indexed = content["indexed"] as! [String]
-        
-        self.store!.save(type, id: id, data: data, indexed: indexed)
-        return [NSObject:AnyObject]()
-    }
-    
-    func deleteObject(operation:String, content:[NSObject:AnyObject]) -> [NSObject:AnyObject]?{
-        let type = content["type"] as! String
-        let id = content["id"] as! String
-        self.store!.delete(type, id: id)
-        return [NSObject:AnyObject]()
-    }
 
-    
-    func loadAllObjects(operation:String, content:[NSObject:AnyObject]) -> [NSObject:AnyObject]?{
-        let type = content["type"] as! String
-        let content = self.store!.loadAll(type)
-        let jsonContent = JSON(content)
-        if let stringResult = jsonContent.rawString() {
-            print(stringResult)
-            return ["data" : stringResult]
-        }
         
-        return [NSObject:AnyObject]()
     }
-    
-    
     
 }
 
